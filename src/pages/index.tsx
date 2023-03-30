ea9f5b1da0b4c2b3e2d72a3a9b902b54a7dbe70b
vishnumohanrk <mohanvish1999@gmail.com>
2023-03-30 19:02:10 +0530

    add title, meta:desc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 
 import { AppHeader } from '@/components/app-header';
 import { BgPattern } from '@/components/bg-pattern';
@@ -12,6 +13,13 @@ const Leaflet = dynamic(() => import('../components/maps'), {
 export default function AppHome() {
   return (
     <>
+      <Head>
+        <title>Frontend Mentor | IP Address Tracker</title>
+        <meta
+          name="description"
+          content="Frontend Mentor Challenge - IP Address Tracker"
+        />
+      </Head>
       <BgPattern />
       <div className="absolute top-0 z-[500] w-full max-xl:px-6">
         <AppHeader />
